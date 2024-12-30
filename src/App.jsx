0325c20039a2014a128afd96602ad5e0949a3fdb
ajithks32 <ajithks5532@gmail.com>
2024-12-30 16:36:29 +0530

    Updated files and added new README

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import About from './Components/About';
+
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />  {/* Display Navbar on every page */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
