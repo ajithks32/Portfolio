0cf64d86c58b03c7be7d77788efeea641fe74fa5
ajithks32 <ajithks5532@gmail.com>
2024-12-30 16:59:01 +0530

    Modified all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
@@ -7,10 +7,9 @@ import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import About from './Components/About';
 
-
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename="/portfolio">  {/* Only use BrowserRouter here */}
       <Navbar />  {/* Display Navbar on every page */}
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +17,7 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
