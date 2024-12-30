ffb66db8943ca2afa73f46d84b5f0138756bf6b7
ajithks32 <ajithks5532@gmail.com>
2024-12-30 21:09:05 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Projects from './Components/Projects';
@@ -11,17 +11,15 @@ import Skill from './Components/Skill';
 
 const App = () => {
   return (
-    <Router>  {/* Only use BrowserRouter here */}
+    <Router> {/* Use HashRouter for GitHub Pages */}
       <Navbar />  {/* Display Navbar on every page */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skill" element={<Skill/>} />
-
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume/>} />
-
       </Routes>
     </Router>
   );
